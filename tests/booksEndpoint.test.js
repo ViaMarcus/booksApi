39c7c3953a2781f51e0f452ec3e20fe7e36d3997
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-06-12 11:41:20 +0200

    adds authentication test and token mock

--- tests/booksEndpoint.test.js
+++ tests/booksEndpoint.test.js
@@ -3,7 +3,7 @@ const supertest = require("supertest");
 const { expect, factory } = require("./test_helper");
 const jsonResponse = require("./jsonResponse");
 
-let server, request, response;
+let server, request, response, token;
 
 before((done) => {
   server = app.listen(done);
@@ -15,9 +15,10 @@ after((done) => {
 });
 
 beforeEach(async () => {
-  const author = await factory.create("Author",
-    { id: 69, name: "J.R.R. Tolkien" }
-  );
+  const author = await factory.create("Author", {
+    id: 69,
+    name: "J.R.R. Tolkien",
+  });
   await factory.createMany("Book", 2, [
     { id: 1, title: "Dune", authorId: author.id },
     { id: 2, title: "The Hobbit", authorId: author.id },
@@ -29,21 +30,35 @@ afterEach(async () => {
 });
 
 describe("GET /api/v1/books", () => {
-  beforeEach(async () => {
-    response = await request.get("/api/v1/books");
+  describe("for authenticated", () => {
+    beforeEach(async () => {
+      await request
+        .post("/api/v1/auth/login")
+        .send({ email: "user@mail.com", password: "password" })
+        .then((response) => {
+          token = response.body.token;
+        });
+      response = await request.get("/api/v1/books").set("Authorization", token);
+    });
+
+    it("responds with status 200", () => {
+      expect(response.status).to.equal(200);
+    });
+
+    it("responds with a collection of books", () => {
+      const expectedBody = {
+        books: [
+          { id: 1, title: "Dune", author: { name: "J.R.R. Tolkien" } },
+          { id: 2, title: "The Hobbit", author: { name: "J.R.R. Tolkien" } },
+        ],
+      };
+      expect(jsonResponse(response)).to.equal(JSON.stringify(expectedBody));
+    });
   });
 
-  it("responds with status 200", () => {
-    expect(response.status).to.equal(200);
-  });
-
-  it("responds with a collection of books", () => {
-    const expectedBody = {
-      books: [
-        { id: 1, title: "Dune", author: { name: "J.R.R. Tolkien" } },
-        { id: 2, title: "The Hobbit", author: { name: "J.R.R. Tolkien" } },
-      ],
-    };
-    expect(jsonResponse(response)).to.equal(JSON.stringify(expectedBody));
+  describe("for non-authenticated", () => {
+    it("should respond with 401", () => {
+      expect(response.status).to.equal(401);
+    });
   });
 });
