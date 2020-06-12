382f7fc81b4410d0979cee4ea0bae99bb8e38701
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-06-12 10:43:22 +0200

    remove unnecessary code from factories, adds nodemon package

--- tests/factories/authorFactory.js
+++ tests/factories/authorFactory.js
@@ -1,7 +1,5 @@
 module.exports = (factory, Models) => {
   factory.define("Author", Models.Author, {
-    name: "Author Name",
-    createdAt: new Date(),
-    updatedAt: new Date()
+    name: "Author Name"
   });
 };
--- tests/factories/bookFactory.js
+++ tests/factories/bookFactory.js
@@ -1,8 +1,6 @@
 module.exports = (factory, Models) => {
   factory.define("Book", Models.Book, {
     title: "Book Title",
-    createdAt: new Date(),
-    updatedAt: new Date(),
     authorId: factory.assoc("Author", "id")
   });
 };
